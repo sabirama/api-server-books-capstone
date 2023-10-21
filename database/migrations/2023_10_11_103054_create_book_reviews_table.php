931e862fa394cf52f5db1bb21d6e265d1fed46ce
sabirama <markcondes21@gmail.com>
2023-10-21 21:31:35 +0800

    fixed routing problem

--- database/migrations/2023_10_11_103054_create_book_reviews_table.php
+++ database/migrations/2023_10_11_103054_create_book_reviews_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->integer('book_id');
             $table->integer('user_id');
             $table->string('body');
-            $table->integer('rate');
+            $table->integer('rate')->nullable();
             $table->timestamps();
         });
     }
